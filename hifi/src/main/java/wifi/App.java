7812cc69517938a188416cc29b6720ccf125ac3f
rakesh <rakesh@gmail.com>
2018-02-24 22:37:11 +0530

    one maven project is added

---
+++ hifi/src/main/java/wifi/App.java
@@ -0,0 +1,13 @@
+package wifi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Father of Nacre  Ranjith Reddy! in java" );
+    }
+}
